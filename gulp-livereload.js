dfb8e928b3c1194ad48adfbc27cf9ea45532df5e
Cyrus <david@jcyr.us>
2014-02-01 04:25:38 +0800

    add some logging

--- gulp-livereload.js
+++ gulp-livereload.js
@@ -1,10 +1,14 @@
 module.exports = function (server) {
   'use strict';
 
-  var Transform = require('stream').Transform,
+  var gutil = require('gulp-util'),
+      path = require('path'),
+      Transform = require('stream').Transform,
       reload = new Transform({objectMode:true});
 
   reload._transform = function(file, encoding, next) {
+    var filename = gutil.colors.magenta(path.basename(file.path));
+    gutil.log(filename + ' was reloaded.');
     server.changed({
       body: {
         files: [file.path]
