3dd5f8e348567fdc2365fba8ae2a78533dc53bd0
Cyrus <david@jcyr.us>
2014-02-05 00:03:35 +0800

    add tests

--- gulp-livereload.js
+++ gulp-livereload.js
@@ -3,8 +3,16 @@ module.exports = function (server) {
 
   var gutil = require('gulp-util'),
       path = require('path'),
+      tinylr = require('tiny-lr'),
       Transform = require('stream').Transform,
       reload = new Transform({objectMode:true});
+
+  if (!(server instanceof tinylr.Server)) {
+    throw new gutil.PluginError(
+      'gulp-livereload',
+      'Please pass an instance of tiny-lr when calling gulp-livereload.'
+    );
+  }
 
   reload._transform = function(file, encoding, next) {
     var filename = gutil.colors.magenta(path.basename(file.path));
---
+++ test/greload.js
@@ -0,0 +1,34 @@
+/*jshint strict:false, unused:false */
+describe('gulp-livereload', function() {
+  var gutil = require('gulp-util'),
+      sinon = require('sinon'),
+      greload = require('../'),
+      tinylr = require('tiny-lr'),
+      should = require('should'),
+      file = new gutil.File({
+      path: '/foo/bar.css'
+    }),
+      server,
+      spy;
+  beforeEach(function() {
+    server = tinylr();
+    spy = sinon.spy(server, 'changed');
+  });
+  it('reloads file passing a livereload server instance', function(done) {
+    var reload = greload(server);
+    reload.end(file);
+    reload.on('data', function(file) {
+      should(spy.calledWith({
+        body: {
+          files: [file.path]
+        }
+      })).ok;
+      done();
+    });
+  });
+  it('emits an error if an livereload server was not passed... for now', function() {
+    should(function() {
+      var reload = greload();
+    }).throw('Please pass an instance of tiny-lr when calling gulp-livereload.');
+  });
+});
