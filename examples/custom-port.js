f7972d961daf1c1db8e9757560e711de97972d5c
Jason Gill <petethechop@gmail.com>
2015-01-27 21:31:38 -0700

    update alternative commented example

--- examples/custom-port.js
+++ examples/custom-port.js
@@ -13,6 +13,6 @@ gulp.task('less', function() {
 
 gulp.task('watch', function() {
   livereload.listen(1234);
-  // livereload.listen({ port: 1234, basePath: 'dist' });
+  // livereload.listen(1234, { basePath: 'dist' });
   gulp.watch('less/*.less', ['less']);
 });
